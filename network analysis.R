944e6ba3d270e056b19abc8768800db656fb60d3
MatusAdamkovic <matho.adamkovic@gmail.com>
2021-05-12 21:50:45 +0200

    Preliminary NA (just an example; everything works except for altering the tuning parameter)

--- network analysis.R
+++ network analysis.R
@@ -19,13 +19,11 @@ corStability(stabIGD)
 plot(stabIGD, statistics = c("Strength","ExpectedInfluence","Closeness", "Betweenness"))
 bootIGD <- bootnet(netIGD, 
                    nBoots = 1000, 
-                   statistics = c("Strength","edge", "EI"),
+                   statistics = c("Strength","edge"),
                    type = "nonparametric",
                    verbose = FALSE) 
-
-#plot centrality CIs
-plot_boot_dybocs_centrality <- plot(boot_dybocs, c("Strength"))
-plot_boot_dybocs_centrality
+plot(bootIGD, c("Strength"))
+plot(bootIGD, c("edge"))
 simIGD <- netSimulator(netIGD,
                         default = "EBICglasso",
                         nCases = c(250,500, 1000, 2000),
@@ -48,22 +46,22 @@ mgmNetIGD <- mgm(select(esports, IGDS9SF_1:IGDS9SF_9),
                  lambdaSel = "EBIC", 
                  lambdaGam = .5, 
                  ruleReg = "AND")
+
 predMgmNetIGD <- predict(mgmNetIGD, select(esports, IGDS9SF_1:IGDS9SF_9))
 predMgmNetIGD$errors
 
-plotMgmNetIGD <- qgraph(mgmNetIGD$pairwise$wadj, 
-                          layout = "spring",
-                          repulsion = 1, 
-                          title = "IGD net",
-                          pie = predMgmNetIGD$errors[,3],
-                          labels = c("1", "2", "3", "4", "5", "6", "7", "8", "9"),
-                          label.cex = 1,
-                          label.scale = FALSE)
+qgraph(mgmNetIGD$pairwise$wadj, 
+       layout = "spring",
+       repulsion = 1, 
+       title = "IGD net",
+       pie = predMgmNetIGD$errors[,3],
+       labels = c("1", "2", "3", "4", "5", "6", "7", "8", "9"),
+       label.cex = 1,
+       label.scale = FALSE)
 
-
-corMat <- cor_auto(select(esports, IGDS9SF_1:IGDS9SF_9))
-Graph_lasso <- qgraph(corMat, graph = "glasso", layout = "spring", tuning = 0,
-                      sampleSize = nrow(esports))
-Graph_lasso2 <- qgraph(corMat, graph = "glasso", layout = "spring", tuning = 0.5,
-                      sampleSize = nrow(esports))
+# corMat <- cor_auto(select(esports, IGDS9SF_1:IGDS9SF_9))
+# Graph_lasso <- qgraph(corMat, graph = "glasso", layout = "spring", tuning = 0,
+#                       sampleSize = nrow(esports))
+# Graph_lasso2 <- qgraph(corMat, graph = "glasso", layout = "spring", tuning = 0.5,
+#                       sampleSize = nrow(esports))
 
